08393cf70d5782ac0679106a2089b0a0fcbc688c
Alex Li <li.alex100@gmail.com>
2017-12-31 14:03:55 +0800

    init mac

---
+++ LogViewerMac/LogViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LogViewer
+//
+//  Created by Alex Li on 31/12/2017.
+//  Copyright © 2017 Alex Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LogViewerMac/LogViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LogViewer
+//
+//  Created by Alex Li on 31/12/2017.
+//  Copyright © 2017 Alex Li. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
